7b3c6b1464d15ea2033e12ab0a0b6e9edd3a0d7b
YoTNT <pikaqiujian@gmail.com>
2020-04-22 01:04:58 -0400

    create manager login feature on web with API

---
+++ ReimbursementApp/src/main/java/dev/jian/services/ReimbursementService.java
@@ -0,0 +1,15 @@
+package dev.jian.services;
+
+import java.util.List;
+
+import dev.jian.entities.Employee;
+import dev.jian.entities.Reimbursement;
+
+public interface ReimbursementService {
+
+	public Reimbursement submitReimbursement(Reimbursement reimbursement);
+	public Reimbursement approveReimbursement(Reimbursement reimbursement);
+	public Reimbursement denyReimbursement(Reimbursement reimbursement);
+	
+	public List<Reimbursement> getReimbursementsByEmployee(Employee employee);
+}
